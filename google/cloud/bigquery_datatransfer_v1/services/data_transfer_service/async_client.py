ebbf5b48aceb1ef75b74974362283bc082420bab
yoshi-automation <yoshi-automation@google.com>
2020-11-17 07:06:03 -0800

    chore: upgrade to gapic-generator 0.35.9
    PiperOrigin-RevId: 339292950
    
    Source-Author: Google APIs <noreply@google.com>
    Source-Date: Tue Oct 27 11:32:46 2020 -0700
    Source-Repo: googleapis/googleapis
    Source-Sha: 07d41a7e5cade45aba6f0d277c89722b48f2c956
    Source-Link: https://github.com/googleapis/googleapis/commit/07d41a7e5cade45aba6f0d277c89722b48f2c956

--- google/cloud/bigquery_datatransfer_v1/services/data_transfer_service/async_client.py
+++ google/cloud/bigquery_datatransfer_v1/services/data_transfer_service/async_client.py
@@ -54,13 +54,57 @@ class DataTransferServiceAsyncClient:
     DEFAULT_ENDPOINT = DataTransferServiceClient.DEFAULT_ENDPOINT
     DEFAULT_MTLS_ENDPOINT = DataTransferServiceClient.DEFAULT_MTLS_ENDPOINT
 
+    data_source_path = staticmethod(DataTransferServiceClient.data_source_path)
+    parse_data_source_path = staticmethod(
+        DataTransferServiceClient.parse_data_source_path
+    )
+    run_path = staticmethod(DataTransferServiceClient.run_path)
+    parse_run_path = staticmethod(DataTransferServiceClient.parse_run_path)
     transfer_config_path = staticmethod(DataTransferServiceClient.transfer_config_path)
     parse_transfer_config_path = staticmethod(
         DataTransferServiceClient.parse_transfer_config_path
     )
 
+    common_billing_account_path = staticmethod(
+        DataTransferServiceClient.common_billing_account_path
+    )
+    parse_common_billing_account_path = staticmethod(
+        DataTransferServiceClient.parse_common_billing_account_path
+    )
+
+    common_folder_path = staticmethod(DataTransferServiceClient.common_folder_path)
+    parse_common_folder_path = staticmethod(
+        DataTransferServiceClient.parse_common_folder_path
+    )
+
+    common_organization_path = staticmethod(
+        DataTransferServiceClient.common_organization_path
+    )
+    parse_common_organization_path = staticmethod(
+        DataTransferServiceClient.parse_common_organization_path
+    )
+
+    common_project_path = staticmethod(DataTransferServiceClient.common_project_path)
+    parse_common_project_path = staticmethod(
+        DataTransferServiceClient.parse_common_project_path
+    )
+
+    common_location_path = staticmethod(DataTransferServiceClient.common_location_path)
+    parse_common_location_path = staticmethod(
+        DataTransferServiceClient.parse_common_location_path
+    )
+
     from_service_account_file = DataTransferServiceClient.from_service_account_file
     from_service_account_json = from_service_account_file
+
+    @property
+    def transport(self) -> DataTransferServiceTransport:
+        """Return the transport used by the client instance.
+
+        Returns:
+            DataTransferServiceTransport: The transport used by the client instance.
+        """
+        return self._client.transport
 
     get_transport_class = functools.partial(
         type(DataTransferServiceClient).get_transport_class,
@@ -156,7 +200,8 @@ class DataTransferServiceAsyncClient:
         # Create or coerce a protobuf request object.
         # Sanity check: If we got a request object, we should *not* have
         # gotten any keyword arguments that map to the request.
-        if request is not None and any([name]):
+        has_flattened_params = any([name])
+        if request is not None and has_flattened_params:
             raise ValueError(
                 "If the `request` argument is set, then none of "
                 "the individual field arguments should be set."
@@ -179,7 +224,7 @@ class DataTransferServiceAsyncClient:
                 maximum=60.0,
                 multiplier=1.3,
                 predicate=retries.if_exception_type(
-                    exceptions.ServiceUnavailable, exceptions.DeadlineExceeded,
+                    exceptions.DeadlineExceeded, exceptions.ServiceUnavailable,
                 ),
             ),
             default_timeout=20.0,
@@ -241,7 +286,8 @@ class DataTransferServiceAsyncClient:
         # Create or coerce a protobuf request object.
         # Sanity check: If we got a request object, we should *not* have
         # gotten any keyword arguments that map to the request.
-        if request is not None and any([parent]):
+        has_flattened_params = any([parent])
+        if request is not None and has_flattened_params:
             raise ValueError(
                 "If the `request` argument is set, then none of "
                 "the individual field arguments should be set."
@@ -264,7 +310,7 @@ class DataTransferServiceAsyncClient:
                 maximum=60.0,
                 multiplier=1.3,
                 predicate=retries.if_exception_type(
-                    exceptions.ServiceUnavailable, exceptions.DeadlineExceeded,
+                    exceptions.DeadlineExceeded, exceptions.ServiceUnavailable,
                 ),
             ),
             default_timeout=20.0,
@@ -348,7 +394,8 @@ class DataTransferServiceAsyncClient:
         # Create or coerce a protobuf request object.
         # Sanity check: If we got a request object, we should *not* have
         # gotten any keyword arguments that map to the request.
-        if request is not None and any([parent, transfer_config]):
+        has_flattened_params = any([parent, transfer_config])
+        if request is not None and has_flattened_params:
             raise ValueError(
                 "If the `request` argument is set, then none of "
                 "the individual field arguments should be set."
@@ -436,7 +483,8 @@ class DataTransferServiceAsyncClient:
         # Create or coerce a protobuf request object.
         # Sanity check: If we got a request object, we should *not* have
         # gotten any keyword arguments that map to the request.
-        if request is not None and any([transfer_config, update_mask]):
+        has_flattened_params = any([transfer_config, update_mask])
+        if request is not None and has_flattened_params:
             raise ValueError(
                 "If the `request` argument is set, then none of "
                 "the individual field arguments should be set."
@@ -509,7 +557,8 @@ class DataTransferServiceAsyncClient:
         # Create or coerce a protobuf request object.
         # Sanity check: If we got a request object, we should *not* have
         # gotten any keyword arguments that map to the request.
-        if request is not None and any([name]):
+        has_flattened_params = any([name])
+        if request is not None and has_flattened_params:
             raise ValueError(
                 "If the `request` argument is set, then none of "
                 "the individual field arguments should be set."
@@ -532,7 +581,7 @@ class DataTransferServiceAsyncClient:
                 maximum=60.0,
                 multiplier=1.3,
                 predicate=retries.if_exception_type(
-                    exceptions.ServiceUnavailable, exceptions.DeadlineExceeded,
+                    exceptions.DeadlineExceeded, exceptions.ServiceUnavailable,
                 ),
             ),
             default_timeout=20.0,
@@ -594,7 +643,8 @@ class DataTransferServiceAsyncClient:
         # Create or coerce a protobuf request object.
         # Sanity check: If we got a request object, we should *not* have
         # gotten any keyword arguments that map to the request.
-        if request is not None and any([name]):
+        has_flattened_params = any([name])
+        if request is not None and has_flattened_params:
             raise ValueError(
                 "If the `request` argument is set, then none of "
                 "the individual field arguments should be set."
@@ -617,7 +667,7 @@ class DataTransferServiceAsyncClient:
                 maximum=60.0,
                 multiplier=1.3,
                 predicate=retries.if_exception_type(
-                    exceptions.ServiceUnavailable, exceptions.DeadlineExceeded,
+                    exceptions.DeadlineExceeded, exceptions.ServiceUnavailable,
                 ),
             ),
             default_timeout=20.0,
@@ -678,7 +728,8 @@ class DataTransferServiceAsyncClient:
         # Create or coerce a protobuf request object.
         # Sanity check: If we got a request object, we should *not* have
         # gotten any keyword arguments that map to the request.
-        if request is not None and any([parent]):
+        has_flattened_params = any([parent])
+        if request is not None and has_flattened_params:
             raise ValueError(
                 "If the `request` argument is set, then none of "
                 "the individual field arguments should be set."
@@ -701,7 +752,7 @@ class DataTransferServiceAsyncClient:
                 maximum=60.0,
                 multiplier=1.3,
                 predicate=retries.if_exception_type(
-                    exceptions.ServiceUnavailable, exceptions.DeadlineExceeded,
+                    exceptions.DeadlineExceeded, exceptions.ServiceUnavailable,
                 ),
             ),
             default_timeout=20.0,
@@ -782,7 +833,8 @@ class DataTransferServiceAsyncClient:
         # Create or coerce a protobuf request object.
         # Sanity check: If we got a request object, we should *not* have
         # gotten any keyword arguments that map to the request.
-        if request is not None and any([parent, start_time, end_time]):
+        has_flattened_params = any([parent, start_time, end_time])
+        if request is not None and has_flattened_params:
             raise ValueError(
                 "If the `request` argument is set, then none of "
                 "the individual field arguments should be set."
@@ -913,7 +965,8 @@ class DataTransferServiceAsyncClient:
         # Create or coerce a protobuf request object.
         # Sanity check: If we got a request object, we should *not* have
         # gotten any keyword arguments that map to the request.
-        if request is not None and any([name]):
+        has_flattened_params = any([name])
+        if request is not None and has_flattened_params:
             raise ValueError(
                 "If the `request` argument is set, then none of "
                 "the individual field arguments should be set."
@@ -936,7 +989,7 @@ class DataTransferServiceAsyncClient:
                 maximum=60.0,
                 multiplier=1.3,
                 predicate=retries.if_exception_type(
-                    exceptions.ServiceUnavailable, exceptions.DeadlineExceeded,
+                    exceptions.DeadlineExceeded, exceptions.ServiceUnavailable,
                 ),
             ),
             default_timeout=20.0,
@@ -989,7 +1042,8 @@ class DataTransferServiceAsyncClient:
         # Create or coerce a protobuf request object.
         # Sanity check: If we got a request object, we should *not* have
         # gotten any keyword arguments that map to the request.
-        if request is not None and any([name]):
+        has_flattened_params = any([name])
+        if request is not None and has_flattened_params:
             raise ValueError(
                 "If the `request` argument is set, then none of "
                 "the individual field arguments should be set."
@@ -1012,7 +1066,7 @@ class DataTransferServiceAsyncClient:
                 maximum=60.0,
                 multiplier=1.3,
                 predicate=retries.if_exception_type(
-                    exceptions.ServiceUnavailable, exceptions.DeadlineExceeded,
+                    exceptions.DeadlineExceeded, exceptions.ServiceUnavailable,
                 ),
             ),
             default_timeout=20.0,
@@ -1075,7 +1129,8 @@ class DataTransferServiceAsyncClient:
         # Create or coerce a protobuf request object.
         # Sanity check: If we got a request object, we should *not* have
         # gotten any keyword arguments that map to the request.
-        if request is not None and any([parent]):
+        has_flattened_params = any([parent])
+        if request is not None and has_flattened_params:
             raise ValueError(
                 "If the `request` argument is set, then none of "
                 "the individual field arguments should be set."
@@ -1098,7 +1153,7 @@ class DataTransferServiceAsyncClient:
                 maximum=60.0,
                 multiplier=1.3,
                 predicate=retries.if_exception_type(
-                    exceptions.ServiceUnavailable, exceptions.DeadlineExceeded,
+                    exceptions.DeadlineExceeded, exceptions.ServiceUnavailable,
                 ),
             ),
             default_timeout=20.0,
@@ -1166,7 +1221,8 @@ class DataTransferServiceAsyncClient:
         # Create or coerce a protobuf request object.
         # Sanity check: If we got a request object, we should *not* have
         # gotten any keyword arguments that map to the request.
-        if request is not None and any([parent]):
+        has_flattened_params = any([parent])
+        if request is not None and has_flattened_params:
             raise ValueError(
                 "If the `request` argument is set, then none of "
                 "the individual field arguments should be set."
@@ -1189,7 +1245,7 @@ class DataTransferServiceAsyncClient:
                 maximum=60.0,
                 multiplier=1.3,
                 predicate=retries.if_exception_type(
-                    exceptions.ServiceUnavailable, exceptions.DeadlineExceeded,
+                    exceptions.DeadlineExceeded, exceptions.ServiceUnavailable,
                 ),
             ),
             default_timeout=20.0,
@@ -1265,7 +1321,8 @@ class DataTransferServiceAsyncClient:
         # Create or coerce a protobuf request object.
         # Sanity check: If we got a request object, we should *not* have
         # gotten any keyword arguments that map to the request.
-        if request is not None and any([name]):
+        has_flattened_params = any([name])
+        if request is not None and has_flattened_params:
             raise ValueError(
                 "If the `request` argument is set, then none of "
                 "the individual field arguments should be set."
@@ -1288,7 +1345,7 @@ class DataTransferServiceAsyncClient:
                 maximum=60.0,
                 multiplier=1.3,
                 predicate=retries.if_exception_type(
-                    exceptions.ServiceUnavailable, exceptions.DeadlineExceeded,
+                    exceptions.DeadlineExceeded, exceptions.ServiceUnavailable,
                 ),
             ),
             default_timeout=20.0,
